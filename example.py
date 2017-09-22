e80076de9969dd294c75d093cc24a2c20151d704
Kensuke Numakura <knmkr3gma@gmail.com>
2017-09-22 13:01:06 -0700

    init

---
+++ example.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import requests  # pip install requests
+
+
+def _main():
+    token = 'GENOMELINKTEST'
+
+    phenotype = 'eye-color'
+    population = 'european'
+    report_url = 'https://genomicexplorer.io/v1/reports/{}?population={}'.format(phenotype, population)
+    response = requests.get(report_url, headers={'Authorization': 'Bearer {}'.format(token)})
+    data = response.json()
+    print json.dumps(data)
+
+    chrom = 'chr1'
+    start = '100000'
+    end = '100500'
+    sequence_url = 'https://genomicexplorer.io/v1/genomes/sequence/?region={}:{}-{}'.format(chrom, start, end)
+    response = requests.get(sequence_url, headers={'Authorization': 'Bearer {}'.format(token)})
+    data = response.json()
+    print json.dumps(data)
+
+if __name__ == '__main__':
+    _main()
